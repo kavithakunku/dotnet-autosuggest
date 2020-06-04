7de755ca27573ce972c92d4d93eedb7ee021533b
kavithakunku <64262411+kavithakunku@users.noreply.github.com>
2020-06-04 15:00:37 +0530

    first

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+//using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace autosuggest
+{
+    class Program
+    {
+        
+        static string host = "https://leela-autosuggest.cognitiveservices.azure.com";
+        static string path = "/bing/v7.0/Suggestions";
+        static string market = "en-US";
+        static string key = "03c40bcea7fc47319d922cce10cd3380";
+
+        static string query = "hi";
+
+        
+        async static void Autosuggest()
+        {
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", key);
+
+            
+            string uri = host + path + "?mkt=" + market + "&query=" + System.Net.WebUtility.UrlEncode(query);
+
+            HttpResponseMessage response = await client.GetAsync(uri);
+
+            string contentString = await response.Content.ReadAsStringAsync();
+            dynamic parsedJson = JsonConvert.DeserializeObject(contentString);
+            Console.WriteLine(JsonConvert.SerializeObject(parsedJson, Formatting.Indented));
+            
+        }
+
+        static void Main(string[] args)
+        {
+            Autosuggest();
+            Console.ReadLine();
+        }
+    }
+}
